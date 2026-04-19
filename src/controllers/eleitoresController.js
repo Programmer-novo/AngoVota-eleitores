b57f6518e1f783f10b97745016997096caa5989b
Programmer-novo <outropc169@gmail.com>
2026-04-19 14:44:40 +0100

    Update eleitoresController.js

--- src/controllers/eleitoresController.js
+++ src/controllers/eleitoresController.js
@@ -73,103 +73,14 @@ class EleitoresController{
 
             const { ativo } = req.body;
 
-            if(!ativo && !req.session.eleitor_id && !req.session.perfilNumberEleitor){
-              delete req.session.eleitor_id;
-              delete req.session.perfilNumberEleitor;
-              return res.status(401).json({error: 'KYC não realizado, ou falhado'});
+            if(!ativo){
+                return res.status(400).json({error: 'Não completou o reconhecimento facial'});
             }
 
-            const bi = req.session.eleitor_id;
+            req.session.KYC = ativo;
 
-            const perfil = req.session.perfilNumberEleitor;
-            const kyc_concluido = ativo
-
-            req.session.kyc = kyc_concluido;
-
-            async function localization(ip) {
-
-                    try{
-  
-                          const token = process.env.Token;
-
-                          const resposta = await fetch(`https://ipinfo.io/${ip}?token=${token}`);
-
-                          const dados = await resposta.json();
-
-                          return{
-                            cidade: dados.city,
-                            provincia: dados.region,
-                            pais: dados.country
-                          };
-
-                          }
-
-                          catch(err){
-                            console.error('Erro ao localizar', err);
-                            return null
-                          }
-                          
-            }
-
-            const ip = req.pegarInformacoes?.ip;
-
-                let listaProvincias = [
-
-                    'Luanda',
-                    'Bengo',
-                    'Benguela',
-                    'Bié',
-                    'Cabinda',
-                    'Cuando Cubango',
-                    'Cuanza Norte',
-                    'Cuanza Sul',
-                    'Cunene',
-                    'Huambo',       
-                    'Huíla',
-                    'Icolo e Bengo',      
-                    'Lunda Norte',
-                    'Lunda Sul',
-                    'Malanje',
-                    'Moxico',
-                    'Moxico Leste',
-                    'Namibe',
-                    'Uíge',
-                    'Zaire'
-             ];
-
-
-            const provincia = listaProvincias[Math.floor(Math.random() * listaProvincias.length)]
-
-
-
-            // Quando estiver em produção
-
-            if (ip !== '127.0.0.0' && ip !== '::1') {
-
-
-                const localizacao = await localization(ip);
-
-                if (localizacao?.provincia) {
-
-
-                    const provincia = localizacao?.provincia;
-
-                }
-                
-            }
-        
-
-            console.log(`${provincia}`);
-
-
-            const criarEleitor = await eleitores.create({bilhete_id:bi, provincia});
-
-             req.session.eleitor_id = criarEleitor.id
-
-             req.session.provincia = criarEleitor.provincia;
-
-            return res.status(200).json({message: 'KYC concluído com sucesso!'});
-
+           
+        return res.status(200).json({message: 'KYC realizado com sucesso!'});
         }catch(error){
             console.log(error)
             return res.status(401).json({message: 'Erro ao validar KYC', error});
@@ -178,6 +89,10 @@ class EleitoresController{
 
      
     }
+
+    
+
+    
 
 
     
